fc1cd1843461a17f0c5d9bacf3f36923d8517e4c
fe9lix <fe9lix@googlemail.com>
2014-02-05 02:35:13 +0100

    Handle interface orientation change in example

--- Plumber/Example/PlumberViewController.m
+++ Plumber/Example/PlumberViewController.m
@@ -1,6 +1,9 @@
 #import "PlumberViewController.h"
+#import "PlumberControl.h"
 
 @interface PlumberViewController ()
+
+@property (weak, nonatomic) IBOutlet PlumberControl *plumberControl;
 
 @end
 
@@ -8,6 +11,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    [super willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    
+    [self.plumberControl setNeedsDisplay];
 }
 
 @end
